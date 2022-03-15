84c0ad927b53e4f012fb3b464af8bebf0d1a0043
anonymous anonymous <mishmosh293@gmail.com>
2022-03-15 01:12:22 +0000

    Remove controller comments

--- app/controllers/squirrels_controller.rb
+++ app/controllers/squirrels_controller.rb
@@ -1,24 +1,19 @@
 class SquirrelsController < ApplicationController
   before_action :set_squirrel, only: %i[show edit update destroy]
 
-  # GET /squirrels
   def index
     @q = Squirrel.ransack(params[:q])
     @squirrels = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
-  # GET /squirrels/1
   def show; end
 
-  # GET /squirrels/new
   def new
     @squirrel = Squirrel.new
   end
 
-  # GET /squirrels/1/edit
   def edit; end
 
-  # POST /squirrels
   def create
     @squirrel = Squirrel.new(squirrel_params)
 
@@ -29,7 +24,6 @@ class SquirrelsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /squirrels/1
   def update
     if @squirrel.update(squirrel_params)
       redirect_to @squirrel, notice: "Squirrel was successfully updated."
@@ -38,7 +32,6 @@ class SquirrelsController < ApplicationController
     end
   end
 
-  # DELETE /squirrels/1
   def destroy
     @squirrel.destroy
     redirect_to squirrels_url, notice: "Squirrel was successfully destroyed."
@@ -46,12 +39,10 @@ class SquirrelsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_squirrel
     @squirrel = Squirrel.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def squirrel_params
     params.require(:squirrel).permit(:name, :description, :age)
   end
